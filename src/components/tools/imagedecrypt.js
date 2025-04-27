17a847537ba1889c8089b7e61625ce33fcf6ae6c
Sanchit Kanwar <123666887+Sanchitkanwar31@users.noreply.github.com>
2025-04-28 01:45:16 +0530

    Update imagedecrypt.js

--- src/components/tools/imagedecrypt.js
+++ src/components/tools/imagedecrypt.js
@@ -23,7 +23,7 @@ const DecryptImage = () => {
 
     try {
       setLoading(true);
-      const response = await fetch('http://127.0.0.1:8000/decode-image', {
+      const response = await fetch('https://esigned-backend.onrender.com/decode-image', {
         method: 'POST',
         body: formData,
       });
