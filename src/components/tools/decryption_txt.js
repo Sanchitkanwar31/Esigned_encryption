031607bf821b75029e34d4e8799f7599c66cc1ca
Sanchit Kanwar <123666887+Sanchitkanwar31@users.noreply.github.com>
2025-04-28 01:43:31 +0530

    Update decryption_txt.js

--- src/components/tools/decryption_txt.js
+++ src/components/tools/decryption_txt.js
@@ -11,7 +11,7 @@ function DecryptPage() {
     
 
     try {
-      const response = await fetch("http://127.0.0.1:8000/decrypt", {
+      const response = await fetch("https://esigned-backend.onrender.com/decrypt", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ text: encryptedText, key }),
